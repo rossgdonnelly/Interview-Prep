02e90d9bc2eb740d600e29636e65d569c3927975
Ross Donnelly <rdonnel5@tcd.ie>
2024-09-06 14:31:11 -0400

    Added python Problem found online to practice dictionary based probs

---
+++ longestConsecutiveCharacter.py
@@ -0,0 +1,31 @@
+def longestConsecutiveCharacter(string):
+    print(string)
+    listStr = list(string)
+    current = listStr[0]
+    maxCount = 0 
+    currentCount = 0
+    i=0
+    max_char = ''
+    while i<len(listStr):
+        current = listStr[i]
+        if (current == listStr[i-1]):
+            currentCount +=1
+            if currentCount>=maxCount:
+                maxCount = currentCount
+                max_char = current
+        else:
+            currentCount = 0
+        i +=1
+    return {max_char: maxCount+1}
+
+      
+
+    
+
+    #Sum up the even numbers
+
+
+if __name__ == '__main__':
+    string = "AABCDDBBBEA"
+    # Function call
+    print("Output: ",longestConsecutiveCharacter(string))
